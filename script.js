982424517d0140e5d13ddf9502eeb5fef4c93df0
James <jamesfulreader@gmail.com>
2020-06-05 12:16:14 -0600

    add functions to script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert('This file is running somthing!');
+function createAlert() {
+    alert('This file is running somthing!');
+}
+
+function addNums() {
+    console.log(1 + 1);
+}
+
+createAlert();
